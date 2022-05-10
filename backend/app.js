d06f21e7d58ff2ead2e68a8814091c1ab8d2e87f
gabrial fancon <gabrialfancon18@gmail.com>
2022-05-11 00:01:06 +0530

    working till now

--- backend/app.js
+++ backend/app.js
@@ -15,7 +15,7 @@ const pickedUpOdersRoutes = require('./routes/pickedUpOders');
 
 
 
-mongoose.connect('mongodb://localhost:27017',{useNewUrlParser: true , useUnifiedTopology: true})
+mongoose.connect('mongodb://localhost:27017/test',{useNewUrlParser: true , useUnifiedTopology: true})
   .then(()=>{
     console.log('connected to database!');
   })
